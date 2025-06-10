1c83504bb8221dcaab4fea918fe6a7f9470de3f0
Ayoub Assis <assis.ayoub@gmail.com>
2025-06-10 11:19:17 +0200

    add gcc-arm-11.2-2022.02-x86_64-aarch64-none-linux-gnu.tar.xz to setup-env.sh
    Signed-off-by: Ayoub Assis <assis.ayoub@gmail.com>

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -14,6 +14,7 @@ apt-get install -y wget curl build-essential
 toolchain_urls=(
     "https://oaax.nbg1.your-objectstorage.com/toolchains/x86_64-unknown-linux-gnu-gcc-9.5.0.tar.gz"
     "https://oaax.nbg1.your-objectstorage.com/toolchains/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu.tar.gz"
+    "https://oaax.nbg1.your-objectstorage.com/toolchains/gcc-arm-11.2-2022.02-x86_64-aarch64-none-linux-gnu.tar.xz"
 )
 
 # Function to extract the filename from a given URL
